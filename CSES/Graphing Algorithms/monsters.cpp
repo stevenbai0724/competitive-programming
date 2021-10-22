dd2fca9dd71eddf5e45f1b40d14f954f495982cd
Steven <stevenbai0724@gmail.com>
2021-10-21 22:17:17 -0400

    10/21/21

--- CSES/Graphing Algorithms/monsters.cpp
+++ CSES/Graphing Algorithms/monsters.cpp
@@ -1,126 +1,205 @@
 //https://cses.fi/problemset/task/1194
-//TLE, 18 out of 21 points
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long
+
 signed main(){
     cin.tie(nullptr)->sync_with_stdio(false);
     
     int n, m;
     cin>>n>>m;
- 
+
     vector<vector<char>>arr(n+1, vector<char>(m+1));
-    vector<pair<int, int>>mon;
-    vector<vector<int>>mDis(n+1, vector<int>(m+1));
-    vector<vector<pair<int, int>>>p(n+1, vector<pair<int, int>>(m+1)); //parent array
-    string ans = "";
-    queue<pair<int, int>>q;
-    vector<int>X{1,-1,0,0};
-    vector<int>Y{0,0,1,-1};
-    pair<int, int>start;
+    vector<vector<int>>dis(n+1, vector<int>(m+1));
+
+    pair<int, int>M_START, A_START;
+    bool first = true;
+
+    vector<vector<vector<pair<int, pair<int, int>>>>>adj(n+1, vector<vector<pair<int, pair<int, int>>>>(m+1));
+
     for(int i=1;i<=n;i++){
         string s; cin>>s;
         for(int j=1;j<=m;j++){
-            mDis[i][j] = 1e18;
             arr[i][j] = s[j-1];
-            if(s[j-1]=='M')mon.push_back({i,j});
-            else if(s[j-1]=='A')start = {i,j};
-        }
-    }
-    for(auto x:mon){
-        vector<vector<int>>dis(n+1, vector<int>(m+1));
-        vector<vector<bool>>vis(n+1, vector<bool>(m+1));
- 
-        q.push(x);  
-        vis[x.first][x.second] = true;
-        mDis[x.first][x.second] = 0;
-        while(!q.empty()){
-            int x = q.front().first;
-            int y = q.front().second;
-            q.pop();
-            for(int i=0;i<4;i++){
-                int newx = x+X[i];
-                int newy = y+Y[i];
-                if(newx>=1 && newx<=n && newy>=1 && newy<=m){
-                    if(arr[newx][newy]!='#' && !vis[newx][newy]){
-                        vis[newx][newy] = true;
-                        q.push({newx, newy});
-                        dis[newx][newy] = dis[x][y] +1;
-                        mDis[newx][newy] = min(mDis[newx][newy], dis[newx][newy]);
-                    }
+            if(arr[i][j] == 'M' ){
+                dis[i][j] = -1;
+                if(first){
+                    first = false;
+                    M_START = {i,j};
                 }
+                else{
+                    adj[M_START.first][M_START.second].push_back({0,{i,j}});
+                }
+            }
+            if(arr[i][j] == 'A'){
+                A_START = {i,j};
             }
         }
     }
-    vector<vector<int>>aDis(n+1, vector<int>(m+1));
-    vector<vector<bool>>aVis(n+1,vector<bool>(m+1));
- 
-    q.push(start);
-    aVis[start.first][start.second] =true;
-    
-    while(!q.empty()){
-        int x = q.front().first;
-        int y = q.front().second;
-        q.pop();
-        for(int i=0;i<4;i++){
-            int newx = x+X[i];
-            int newy = y+Y[i];
-            if(newx>=1 && newx<=n && newy>=1 && newy<=m){
-                if(arr[newx][newy]!='#' && !aVis[newx][newy] && aDis[x][y]+1<mDis[newx][newy]){
-                    aDis[newx][newy] = aDis[x][y]+1;
-                    aVis[newx][newy] = true;
-                    p[newx][newy] = {x,y};
-                    q.push({newx, newy});
-                }
-            }
-        }
-    }
-    bool no = true;
-    pair<int, int>end;
     for(int i=1;i<=n;i++){
         for(int j=1;j<=m;j++){
-            if(i==1 || i==n || j==1 || j==m){
-                if(aVis[i][j]){
-                    no = false;
-                    end = {i,j};
- 
-                }
+            if(arr[i][j]=='A' && (i==1 || i==n || j==1 || j==m)){
+                cout<<"YES\n0\n";
+                return 0;
             }
-            if(!no)break;
+            if(dis[i][j]==0)dis[i][j] = 1e18;
+            if(dis[i][j]==-1)dis[i][j] = 0;
+            if(arr[i][j]=='#')continue;
+            if(i-1>=1 && arr[i-1][j]!='#') adj[i][j].push_back({1,{i-1,j}});
+            if(i+1<=n && arr[i+1][j]!='#') adj[i][j].push_back({1,{i+1,j}});
+            if(j-1>=1 && arr[i][j-1]!='#') adj[i][j].push_back({1,{i,j-1}});
+            if(j+1<=m && arr[i][j+1]!='#') adj[i][j].push_back({1,{i,j+1}});
         }
-        if(!no)break;
-    }   
-    if(no){
-        cout<<"NO";
+    }
+    bool good = false;
+    int X = A_START.first;
+    int Y = A_START.second;
+
+    if(X+1<=n){
+        if((arr[X+1][Y]=='.'))good = true;
+    }
+    if(X-1>=1){
+        if((arr[X-1][Y]=='.'))good = true;
+    }
+    if(Y+1<=m){
+        if((arr[X][Y+1]=='.'))good = true;
+    }
+    if(Y-1>=1){
+        if((arr[X][Y-1]=='.'))good = true;
+    }
+    if(!good){
+        cout<<"NO\n";
         return 0;
     }
-    int curx = end.first;
-    int cury = end.second;
-    vector<char>vec;
- 
- 
-    while(aDis[curx][cury]!=0){
-        int newx = p[curx][cury].first;
-        int newy = p[curx][cury].second;
- 
-        if(newx<curx)vec.push_back('D');
-        if(newx>curx)vec.push_back('U');
-        if(newy<cury)vec.push_back('R');
-        if(newy>cury)vec.push_back('L');
+
+    priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>>q;
+   
+    q.push({0, M_START});
+
+    while(!q.empty()){
+        int w = q.top().first;
+        int x = q.top().second.first;
+        int y = q.top().second.second;
+        q.pop();
+        if(dis[x][y]<w)continue;
+        for(auto p: adj[x][y]){
+            int neww = p.first;
+            int newx = p.second.first;
+            int newy = p.second.second;
+            if(dis[newx][newy] >= dis[x][y] + neww){
+                dis[newx][newy] = dis[x][y] + neww;
+                q.push({dis[newx][newy], {newx, newy}});
+            }
+        }
+    }
+
+    vector<vector<int>>run(n+1, vector<int>(m+1));
+    vector<vector<bool>>vis(n+1, vector<bool>(m+1));
+    vector<vector<pair<int, int>>>parent(n+1, vector<pair<int, int>>(m+1));
+    vector<int>dx{0,0,1,-1};
+    vector<int>dy{-1,1,0,0};
+    queue<pair<int, int>>qu;
+    qu.push(A_START);
+    vis[A_START.first][A_START.second] = true;
+    while(!qu.empty()){
+        int curx = qu.front().first;
+        int cury = qu.front().second;
+        qu.pop();
+
         
-        curx = newx;
-        cury = newy;
+        for(auto p: adj[curx][cury]){
+            int newx = p.second.first;
+            int newy = p.second.second;
+            if(dis[newx][newy]>run[curx][cury]+1 && !vis[newx][newy]){
+                vis[newx][newy] = true;
+                run[newx][newy] = run[curx][cury] + 1;
+                parent[newx][newy] = {curx, cury};
+                qu.push({newx, newy});
+            }
+        }
     }
- 
- 
-    reverse(vec.begin(), vec.end());
-    for(auto x:vec){
-        ans+=x;
+    string ans = "";
+    for(int j=1;j<=m;j++){
+        if(vis[1][j]){
+            cout<<"YES\n";
+            pair<int, int>cur = {1,j};
+            pair<int, int>nxt = {1,j};
+
+            while(true){
+                nxt = parent[cur.first][cur.second];
+                if(nxt.first<cur.first)ans += 'D';
+                if(nxt.first>cur.first)ans += 'U';
+                if(nxt.second<cur.second)ans += 'R';
+                if(nxt.second>cur.second)ans += 'L';
+                if(nxt == A_START)break;
+                cur = nxt;
+            }
+            reverse(ans.begin(), ans.end());
+            cout<<ans.length()<<"\n";
+            cout<<ans<<"\n";
+            return 0;
+
+        }
+        if(vis[n][j]){
+            cout<<"YES\n";
+            pair<int, int>cur = {n,j};
+            pair<int, int>nxt = {n,j};
+
+            while(true){
+                nxt = parent[cur.first][cur.second];
+                if(nxt.first<cur.first)ans += 'D';
+                if(nxt.first>cur.first)ans += 'U';
+                if(nxt.second<cur.second)ans += 'R';
+                if(nxt.second>cur.second)ans += 'L';
+                if(nxt == A_START)break;
+                cur = nxt;
+            }
+            reverse(ans.begin(), ans.end());
+            cout<<ans.length()<<"\n";
+            cout<<ans<<"\n";
+            return 0;
+        }
     }
-    cout<<"YES\n"<<vec.size()<<"\n"<<ans;
-    
- 
- 
- 
+    for(int i=1;i<=n;i++){
+        if(vis[i][1]){
+            cout<<"YES\n";
+            pair<int, int>cur = {i,1};
+            pair<int, int>nxt = {i,1};
+
+            while(true){
+                nxt = parent[cur.first][cur.second];
+                if(nxt.first<cur.first)ans += 'D';
+                if(nxt.first>cur.first)ans += 'U';
+                if(nxt.second<cur.second)ans += 'R';
+                if(nxt.second>cur.second)ans += 'L';
+                if(nxt == A_START)break;
+                cur = nxt;
+            }
+            reverse(ans.begin(), ans.end());
+            cout<<ans.length()<<"\n";
+            cout<<ans<<"\n";
+            return 0;
+        }
+        if(vis[i][m]){
+            cout<<"YES\n";
+            pair<int, int>cur = {i,m};
+            pair<int, int>nxt = {i,m};
+
+            while(true){
+                nxt = parent[cur.first][cur.second];
+                if(nxt.first<cur.first)ans += 'D';
+                if(nxt.first>cur.first)ans += 'U';
+                if(nxt.second<cur.second)ans += 'R';
+                if(nxt.second>cur.second)ans += 'L';
+                if(nxt == A_START)break;
+                cur = nxt;
+            }
+            reverse(ans.begin(), ans.end());
+            cout<<ans.length()<<"\n";
+            cout<<ans<<"\n";
+            return 0;
+        }
+    }
+    cout<<"NO\n";
     return 0;
 }
