9d8317bc03a482b80ad547a65419fa40b14ca44a
Steven <stevenbai0724@gmail.com>
2021-12-28 12:39:02 -0500

    12/28/21

--- Algorithms & Data Structures/Graph/dijikstra.cpp
+++ Algorithms & Data Structures/Graph/dijikstra.cpp
@@ -16,8 +16,6 @@ signed main(){
     vector<vector<pair<int, int>>>adj(n+1);
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>>q;
 
-
-
     for(int i=1;i<=m;i++){
         int x, y, w;
         cin>>x>>y>>w;
@@ -25,33 +23,32 @@ signed main(){
         adj[y].push_back({w,x});
     }
 
-    vector<int>dis(n+1, 1e18);
-
-    dis[1] = 0;
-
     q.push({0,1});
+
+    vector<int>dis(n+1, 1e18);
+    vector<bool>vis(n+1, false);
+    dis[1] = 0;
 
     while(!q.empty()){
         int w = q.top().first;
         int y = q.top().second;
         q.pop();
-        if(dis[y]<w)continue;
-
+        if(vis[y])continue;
+        vis[y] = true;
+        
         for(auto nxt: adj[y]){
             int neww = nxt.first;
             int newy = nxt.second;
-            if(dis[y]+neww < dis[newy]){
+            if(dis[y] + neww < dis[newy]){
                 dis[newy] = dis[y] + neww;
                 q.push({dis[newy], newy});
             }
         }
     }
+
     for(int i=1;i<=n;i++){
-        if(dis[i] == 1e18)cout<<-1<<"\n";
-        else cout<<dis[i]<<"\n";
+        cout<<(dis[i] == 1e18 ? -1 : dis[i])<<"\n";
     }
-
-
 
     return 0;
 }
